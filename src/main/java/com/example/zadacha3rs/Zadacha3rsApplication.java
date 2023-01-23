fe2e3d3f71d0d57130ed748ca7a882b124223252
SATodorov18 <SATodorov18@codingburgas.bg>
2023-01-23 11:33:00 +0200

    intial commit

---
+++ src/main/java/com/example/zadacha3rs/Zadacha3rsApplication.java
@@ -0,0 +1,13 @@
+package com.example.zadacha3rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadacha3rsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadacha3rsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadacha3rs/Zadacha3rsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadacha3rs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadacha3rsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
